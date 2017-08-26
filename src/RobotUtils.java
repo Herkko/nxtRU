6b2549a88c9345e21be9d85e5e97761a826c0085
Myllikä <you@example.com>
2017-08-26 12:10:21 +0300

    utilsfunktion muutokset

--- src/RobotUtils.java
+++ src/RobotUtils.java
@@ -1,44 +1,50 @@
 import lejos.nxt.LCD;
 import lejos.nxt.Motor;
 import lejos.nxt.Button;
-
+import lejos.nxt.TouchSensor;
+import lejos.nxt.SensorPort;
 
 public class RobotUtils {
+	TouchSensor touch;
 	
 	public RobotUtils(){
-		
+		TouchSensor touch = new TouchSensor(SensorPort.S1);	
 		
 	}
 
 	
 	
 	
-	public void liikuEteen() {
+	public void moveForward() {
 		Motor.A.forward();
 		Motor.B.forward();
 	}
 	
-	public void liikuTaakse() {
+	public void moveBack() {
 		Motor.A.backward();
 		Motor.B.backward();
 		
 	}
 	
-	public void kaannyVasemmalle(int astemaara) {
+	public void turnLef(int degree) {
 		Motor.A.forward();
 		
 	}
 	
-	public void kaannyOikealle(int astemaara ) {
+	public void turnRight(int degree ) {
 		Motor.B.forward();
 	}
 	
-	public void kauhaKiinni() {
+	public void closeClaW() {
 		Motor.C.rotate(90);
 		
 	}
-	public void kauhaAuki() {
+	public void openClaw() {
 		Motor.C.rotate(90);
+	}
+	
+	public boolean touchSensorIsPressed() {
+	return touch.isPressed();
 	}
 	
 	
