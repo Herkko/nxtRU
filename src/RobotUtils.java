10fe33aeb1cf613086bc08f2b3e8aa18ffae5540
Myllikä <you@example.com>
2017-08-26 11:16:14 +0300

    kauhafunktiot

--- src/RobotUtils.java
+++ src/RobotUtils.java
@@ -1,4 +1,5 @@
 import lejos.nxt.LCD;
+import lejos.nxt.Motor;
 import lejos.nxt.Button;
 
 
@@ -6,12 +7,22 @@ public class RobotUtils {
 	
 	public RobotUtils(){
 		
+		
 	}
 
 	public void showMessage() {
 		LCD.drawInt(7337, 0, 0);
 		Button.waitForAnyPress();
 		
+	}
+	public void kauhaKiinni() {
+		Motor.C.rotate(90);
+		
+		
+		
+	}
+	public void kauhaAuki() {
+		Motor.C.rotate();
 	}
 	
 	
